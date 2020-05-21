5270db2032d86287b94c25aec2a366daaf2d08f4
Crowdbotics <team@crowdbotics.com>
2020-05-21 12:25:14 +0000

    Update admin title

--- tweenty_1st_may_dev_4901/urls.py
+++ tweenty_1st_may_dev_4901/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Chetna  New2"
-admin.site.site_title = "Chetna  New2 Admin Portal"
-admin.site.index_title = "Chetna  New2 Admin"
+admin.site.site_header = "Test 21st"
+admin.site.site_title = "Test 21st Admin Portal"
+admin.site.index_title = "Test 21st Admin"
 
 # swagger
 schema_view = get_schema_view(
